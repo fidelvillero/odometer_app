cd2898aef3c445ee49aefaa822d1019d4cc16cdd
fidel jose villero de hoyos <fidel@dinamic-tigo186-97-53-172.tigo.com.co>
2011-10-09 18:45:13 -0500

    obtener la id_stacion mas concurrida por el visitante actual

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -9,17 +9,35 @@ class Users::ProfilesController < ApplicationController
       @user = User.new
     end
     
+    def each_services_id(hash_state_current, hash_ids_station_distinct)
+      p "fidel"*100
+      p @count_state_mas_frecuentada = hash_state_current[1]
+      hash_ids_station_distinct.each do |ids|
+        if hash_state_current[ids.station_id] > @count_state_mas_frecuentada
+          p "la estacion con state_id " + ids.station_id.to_s + " es mayor"
+          @count_state_mas_frecuentada = hash_state_current[ids.station_id]
+          p "la mayor concurrencia es : "
+          p @count_state_mas_frecuentada
+        end
+      end
+    end
+    
     def show
       @user_show = current_user
-      #minimo coste de gas, de mi consumo
-      @cheapest_coste_gas_last_three_month = current_user.services.minimum('average_costo_gal', :conditions => ['Date > ?', '(Time.new + 3.month)'])
-      #promedio de coste de gasolina en las estaciones visitadas por mi
-      @state_more_visited = current_user.services.average(:costo_gas, :conditions => ['Date > ?', '(Time.new + 3.month)'])
-      #@state_more_visited = @user_show.services.maximum('station_id')
-      @name_station_more_visited = Station.where(:id => @name_station_more_visited)
-      
-      
       @car_pertinente = current_user.car
+
+      p "todas las station_id" + "<"*100
+      #p @user_show.services(:station_id).count
+      #las estaciones q mas frecuento
+      p "*" * 100
+      @station_concurrente = @user_show.services.count(:station_id, :group => :station_id)
+      #@station_concurrente = @user_show.services.count(:station_id, :group => :station_id).to_hash
+      p  @station_concurrente#.find[:station_id]
+      p "s"*100
+      @hash_ids_station_distinct = @user_show.services.select('DISTINCT station_id')
+  
+      
+      each_services_id(@station_concurrente, @hash_ids_station_distinct)
       
       data_table = GoogleVisualr::DataTable.new
       # Add Column Headers
